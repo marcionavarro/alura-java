89f41bbfe5a100b9af6ecae9a1c534601a8d8855
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-07-24 18:01:50 -0300

    02-Aplicando a Orientação a Objetos[ 03-Reaproveitando características e comportamentos ] Finalizado

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculo.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,6 +8,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -16,5 +19,25 @@ public class Principal {
         // meuFilme.somaDasAvaliacoes = 10;
         // meuFilme.totalDeAvaliacoes = 1;
         // System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos() + " minutos");
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calc = new CalculadoraDeTempo();
+        calc.inclui(meuFilme);
+        calc.inclui(outroFilme);
+        calc.inclui(lost);
+        System.out.println(calc.getTempoTotal());
     }
 }
