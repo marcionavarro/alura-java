5683078951953b32765656675a712f1b7bc80a49
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-04-23 22:41:09 -0300

    02-Java Web crie aplicações usando Spring Boot [ 01-Trabalhando com lambdas, streams e Spring Framework ] - 01-Um novo projeto utilizando o Spring Framework

--- screenmatch-2/src/main/java/br/com/mnfullstack/screenmatch/ScreenmatchApplication.java
+++ screenmatch-2/src/main/java/br/com/mnfullstack/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,8 @@
 package br.com.mnfullstack.screenmatch;
 
+import br.com.mnfullstack.screenmatch.model.DadosSerie;
+import br.com.mnfullstack.screenmatch.service.ConsumoApi;
+import br.com.mnfullstack.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=");
+		/*System.out.println(json);
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");*/
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
---
+++ screenmatch-2/src/main/java/br/com/mnfullstack/screenmatch/service/IConverteDados.java
@@ -0,0 +1,7 @@
+package br.com.mnfullstack.screenmatch.service;
+
+public interface IConverteDados {
+
+    <T> T obterDados(String json, Class<T> classe);
+
+}
