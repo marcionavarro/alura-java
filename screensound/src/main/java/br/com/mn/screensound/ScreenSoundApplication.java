4484ae98128188547d46330bc11e9e99f806fb45
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-05-12 21:23:05 -0300

    02-Java Web crie aplicações usando Spring Boot [ 02-persistência de dados e consultas com Spring Data JPA ] - 06-Consolidando conhecimentos - Configurando as bases do projeto

---
+++ screensound/src/main/java/br/com/mn/screensound/ScreenSoundApplication.java
@@ -0,0 +1,20 @@
+package br.com.mn.screensound;
+
+import br.com.mn.screensound.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenSoundApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+		principal.exibeMenu();
+    }
+}
