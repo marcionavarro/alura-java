119c567a48141b3c60323199d316423f1de7faff
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-05-13 21:20:29 -0300

    02-Java Web crie aplicações usando Spring Boot [ 03-criando sua primeira API e conectando ao front ] - 02-Fornecendo dados para o front

---
+++ screenmatch-web/java-screenmatch-web/src/main/java/br/com/alura/screenmatch/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5501/")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
+    }
+
+}
