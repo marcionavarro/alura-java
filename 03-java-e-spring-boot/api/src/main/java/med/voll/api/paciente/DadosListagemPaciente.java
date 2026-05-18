81afb53acc157b77db9b17a42926d46c2d960e54
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-05-17 22:39:00 -0300

    03-Java e Spring Boot [ 01-Spring Boot 3 desenvolva uma API Rest em Java ] Adicionado Pacote Paciente

---
+++ 03-java-e-spring-boot/api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,13 @@
+package med.voll.api.paciente;
+
+public record DadosListagemPaciente(
+    Long id,
+    String nome,
+    String cpf,
+    String email
+) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getEmail());
+    }
+}
---
+++ 03-java-e-spring-boot/api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,50 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity
+@Table(name = "pacientes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativo;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
+    }
+
+    public void atualizarInformacoes(@Valid DadosAtualizarPaciente dados) {
+        if (dados.nome() != null) this.nome = dados.nome();
+        if (dados.telefone() != null) this.telefone = dados.telefone();
+        if (dados.endereco() != null) this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
