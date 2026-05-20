dadceb3e5499379d7ae495a5f48472174b6a40bc
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-05-20 17:00:49 -0300

    03-Java e Spring Boot [ 02-Spring Boot 3 aplique boas práticas e proteja uma API Rest ] 03-Spring Security

--- 03-java-e-spring-boot/api/src/main/java/med/voll/api/ApiApplication.java
+++ 03-java-e-spring-boot/api/src/main/java/med/voll/api/ApiApplication.java
@@ -2,11 +2,13 @@ package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
---
+++ 03-java-e-spring-boot/api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,48 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String senha;
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+}
