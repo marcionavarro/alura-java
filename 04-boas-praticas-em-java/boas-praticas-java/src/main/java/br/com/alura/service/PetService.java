6b3bded1563c6a8a295b6509c036481c4cc41060
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-26 00:07:50 -0300

    04-Boas Práticas em Java [ 01-Java e refatoração melhorando códigos com boas práticas ] 04-Testes automatizados

--- 04-boas-praticas-em-java/boas-praticas-java/src/main/java/br/com/alura/service/PetService.java
+++ 04-boas-praticas-em-java/boas-praticas-java/src/main/java/br/com/alura/service/PetService.java
@@ -4,17 +4,10 @@ import br.com.alura.client.ClientHttpConfiguration;
 import br.com.alura.domain.Pet;
 import br.com.alura.domain.TipoPet;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
 import java.util.List;
