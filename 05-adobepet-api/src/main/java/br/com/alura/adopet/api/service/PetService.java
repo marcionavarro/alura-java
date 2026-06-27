3daef00479684d060ba249bdab91a4f103ac6286
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-27 15:26:32 -0300

    04-Boas Práticas em Java [ 02-Boas práticas de programação melhore o código de uma API Java ] 05-Desafios - Refatorar Pet

---
+++ 05-adobepet-api/src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -0,0 +1,24 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.PetDTO;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PetService {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public List<PetDTO> buscarPetsDisponiveis() {
+        return petRepository
+            .findAllByAdotadoFalse()
+            .stream()
+            .map(PetDTO::new)
+            .toList();
+    }
+
+}
