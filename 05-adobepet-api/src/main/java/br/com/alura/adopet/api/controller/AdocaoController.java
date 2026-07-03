e31a302be7d19f1a1d6a9269ef20a2f35fe3222d
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-07-02 22:23:28 -0300

    04-Boas Práticas em Java [ 03-Boas práticas de programação automatizando testes com Java ] 05-Testes automatizados em Controllers

--- 05-adobepet-api/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ 05-adobepet-api/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdocaoController {
 
     @Autowired
-    AdocaoService adocaoService;
+    private AdocaoService adocaoService;
 
     @PostMapping
     @Transactional
